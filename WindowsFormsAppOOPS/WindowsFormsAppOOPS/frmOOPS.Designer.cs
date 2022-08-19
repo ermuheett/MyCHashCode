dcb0bb046fad0379eaa8d298b053561937c49803
Muheet Ahmed Butt <111588538+ermuheett@users.noreply.github.com>
2022-08-20 00:45:27 +0530

    Add files via upload

---
+++ WindowsFormsAppOOPS/WindowsFormsAppOOPS/frmOOPS.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsAppOOPS
+{
+    partial class frmOOPS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butnStaticVar = new System.Windows.Forms.Button();
+            this.butnStaticFunc = new System.Windows.Forms.Button();
+            this.butnInherit = new System.Windows.Forms.Button();
+            this.butnMultiInher = new System.Windows.Forms.Button();
+            this.butnPolyMor = new System.Windows.Forms.Button();
+            this.butnStaticClass = new System.Windows.Forms.Button();
+            this.butnParClass = new System.Windows.Forms.Button();
+            this.butnAilas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butnStaticVar
+            // 
+            this.butnStaticVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnStaticVar.Location = new System.Drawing.Point(12, 12);
+            this.butnStaticVar.Name = "butnStaticVar";
+            this.butnStaticVar.Size = new System.Drawing.Size(129, 60);
+            this.butnStaticVar.TabIndex = 0;
+            this.butnStaticVar.Text = "Static Variables";
+            this.butnStaticVar.UseVisualStyleBackColor = true;
+            this.butnStaticVar.Click += new System.EventHandler(this.butnStaticVar_Click);
+            // 
+            // butnStaticFunc
+            // 
+            this.butnStaticFunc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnStaticFunc.Location = new System.Drawing.Point(12, 78);
+            this.butnStaticFunc.Name = "butnStaticFunc";
+            this.butnStaticFunc.Size = new System.Drawing.Size(129, 60);
+            this.butnStaticFunc.TabIndex = 1;
+            this.butnStaticFunc.Text = "Static Function";
+            this.butnStaticFunc.UseVisualStyleBackColor = true;
+            this.butnStaticFunc.Click += new System.EventHandler(this.butnStaticFunc_Click);
+            // 
+            // butnInherit
+            // 
+            this.butnInherit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnInherit.Location = new System.Drawing.Point(12, 144);
+            this.butnInherit.Name = "butnInherit";
+            this.butnInherit.Size = new System.Drawing.Size(129, 60);
+            this.butnInherit.TabIndex = 2;
+            this.butnInherit.Text = "Inheritance";
+            this.butnInherit.UseVisualStyleBackColor = true;
+            this.butnInherit.Click += new System.EventHandler(this.butnInherit_Click);
+            // 
+            // butnMultiInher
+            // 
+            this.butnMultiInher.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnMultiInher.Location = new System.Drawing.Point(12, 210);
+            this.butnMultiInher.Name = "butnMultiInher";
+            this.butnMultiInher.Size = new System.Drawing.Size(129, 60);
+            this.butnMultiInher.TabIndex = 3;
+            this.butnMultiInher.Text = "Multi Inheritance";
+            this.butnMultiInher.UseVisualStyleBackColor = true;
+            this.butnMultiInher.Click += new System.EventHandler(this.butnMultiInher_Click);
+            // 
+            // butnPolyMor
+            // 
+            this.butnPolyMor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnPolyMor.Location = new System.Drawing.Point(12, 276);
+            this.butnPolyMor.Name = "butnPolyMor";
+            this.butnPolyMor.Size = new System.Drawing.Size(129, 60);
+            this.butnPolyMor.TabIndex = 4;
+            this.butnPolyMor.Text = "Polymorphism";
+            this.butnPolyMor.UseVisualStyleBackColor = true;
+            this.butnPolyMor.Click += new System.EventHandler(this.butnPolyMor_Click);
+            // 
+            // butnStaticClass
+            // 
+            this.butnStaticClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnStaticClass.Location = new System.Drawing.Point(12, 342);
+            this.butnStaticClass.Name = "butnStaticClass";
+            this.butnStaticClass.Size = new System.Drawing.Size(129, 60);
+            this.butnStaticClass.TabIndex = 5;
+            this.butnStaticClass.Text = "Static Class";
+            this.butnStaticClass.UseVisualStyleBackColor = true;
+            this.butnStaticClass.Click += new System.EventHandler(this.butnStaticClass_Click);
+            // 
+            // butnParClass
+            // 
+            this.butnParClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnParClass.Location = new System.Drawing.Point(147, 12);
+            this.butnParClass.Name = "butnParClass";
+            this.butnParClass.Size = new System.Drawing.Size(129, 60);
+            this.butnParClass.TabIndex = 6;
+            this.butnParClass.Text = "Partial Class";
+            this.butnParClass.UseVisualStyleBackColor = true;
+            this.butnParClass.Click += new System.EventHandler(this.butnParClass_Click);
+            // 
+            // butnAilas
+            // 
+            this.butnAilas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butnAilas.Location = new System.Drawing.Point(147, 78);
+            this.butnAilas.Name = "butnAilas";
+            this.butnAilas.Size = new System.Drawing.Size(129, 60);
+            this.butnAilas.TabIndex = 7;
+            this.butnAilas.Text = "Alias";
+            this.butnAilas.UseVisualStyleBackColor = true;
+            this.butnAilas.Click += new System.EventHandler(this.butnAilas_Click);
+            // 
+            // frmOOPS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 714);
+            this.Controls.Add(this.butnAilas);
+            this.Controls.Add(this.butnParClass);
+            this.Controls.Add(this.butnStaticClass);
+            this.Controls.Add(this.butnPolyMor);
+            this.Controls.Add(this.butnMultiInher);
+            this.Controls.Add(this.butnInherit);
+            this.Controls.Add(this.butnStaticFunc);
+            this.Controls.Add(this.butnStaticVar);
+            this.Name = "frmOOPS";
+            this.Text = "Working with OOPS";
+            this.Load += new System.EventHandler(this.frmOOPS_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butnStaticVar;
+        private System.Windows.Forms.Button butnStaticFunc;
+        private System.Windows.Forms.Button butnInherit;
+        private System.Windows.Forms.Button butnMultiInher;
+        private System.Windows.Forms.Button butnPolyMor;
+        private System.Windows.Forms.Button butnStaticClass;
+        private System.Windows.Forms.Button butnParClass;
+        private System.Windows.Forms.Button butnAilas;
+    }
+}
+
---
+++ WindowsFormsAppOOPS/WindowsFormsAppOOPS/frmOOPS.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using TestAlias = System.Text.StringBuilder;
+
+
+namespace WindowsFormsAppOOPS
+{
+    public partial class frmOOPS : Form
+    {
+        public frmOOPS()
+        {
+            InitializeComponent();
+        }
+
+        private void butnStaticVar_Click(object sender, EventArgs e)
+        {
+
+
+
+            StaticVarEx objNum1 = new StaticVarEx();
+            StaticVarEx objNum2 = new StaticVarEx();
+
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+            
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+
+
+
+            MessageBox.Show("The Count of Number 1 is : " + StaticVarEx.getVal().ToString());
+
+            
+
+
+
+        }
+
+        private void butnStaticFunc_Click(object sender, EventArgs e)
+        {
+
+
+            StaticVarEx objNum1 = new StaticVarEx();
+            StaticVarEx objNum2 = new StaticVarEx();
+
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+            objNum1.count();
+
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+            objNum2.count();
+
+
+
+            MessageBox.Show("The Count of Number 1 is : " + StaticVarEx.getVal().ToString());
+
+            }
+
+        private void frmOOPS_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butnInherit_Click(object sender, EventArgs e)
+        {
+
+
+            Rectangle  objRect = new Rectangle ();
+
+
+            objRect.SetHeight(10);
+            objRect.setWidth(10);
+
+
+           MessageBox .Show ("The Area of RectAngle is " +  objRect.getArea().ToString());
+
+
+
+
+
+
+
+
+
+        }
+
+        private void butnMultiInher_Click(object sender, EventArgs e)
+        {
+
+
+            DerivedTest objTest = new DerivedTest();
+
+            objTest.setLength(10);
+            objTest.setWidth(20);
+
+            MessageBox.Show("THe Areaa is " + objTest.getVal());
+
+
+
+
+
+        }
+
+        private void butnPolyMor_Click(object sender, EventArgs e)
+        {
+
+
+            Box bxA = new Box();
+            Box bxB = new Box();
+            Box bxC = new Box();
+
+
+            // Set Values for Boxes
+
+
+            bxA.setBreadth (10);
+            bxA.setHeight (10);
+            bxA.setLength (10);
+            MessageBox.Show("The Volume of Box 1 is " + bxA.getVolume().ToString());
+
+
+            bxB.setBreadth(10);
+            bxB.setHeight(10);
+            bxB.setLength(10);
+            MessageBox.Show("The Volume of Box 2 is " + bxB.getVolume().ToString());
+
+
+            bxC = bxA + bxB;
+            MessageBox.Show("The Volume of Box 3 is " + bxC.getVolume().ToString());
+
+            
+        }
+
+        private void butnStaticClass_Click(object sender, EventArgs e)
+        {
+
+
+           
+            TestStatic.printMessage ();
+
+
+
+        }
+
+        private void butnParClass_Click(object sender, EventArgs e)
+        {
+
+    
+
+        }
+
+        private void butnAilas_Click(object sender, EventArgs e)
+        {
+
+
+            TestAlias tstTest = new TestAlias();
+
+            tstTest.Append("Hello");
+
+            MessageBox.Show(tstTest.ToString());
+
+        }
+
+    }
+
+
+    
+
+    // Static Class Code
+
+
+    static class TestStatic
+    {
+
+        public static void printMessage()
+        {
+
+            MessageBox.Show("I am Printing this Message");
+
+
+        }
+    }
+
+
+
+
+    // Code for Poly MorPhism
+
+    class Box
+    {
+
+
+        private double dblLength;
+        private double dblHeight;
+        private double dblBreadth;
+
+
+
+        public void setLength(double dblL)
+        {
+            dblLength = dblL;
+
+        }
+
+
+        public void setHeight(double dblH)
+        {
+
+            dblHeight = dblH;
+        }
+
+        public void setBreadth(double dblB)
+        {
+
+            dblBreadth = dblB;
+
+
+        }
+
+        public double getVolume()
+        {
+
+            return (dblBreadth * dblLength * dblHeight);
+        }
+    
+    
+        // Overload + operator to add Boxes
+
+        public static Box operator+ (Box b, Box c)
+        {
+
+
+            Box box = new Box();
+
+            box.dblLength = b.dblLength + c.dblLength;
+            box.dblBreadth = b.dblBreadth + c.dblBreadth;
+            box.dblHeight = b.dblHeight + c.dblHeight;
+
+            return box;
+
+
+        
+        }
+
+    }
+
+
+
+
+
+    class StaticVarEx
+    {
+        public static int intA;
+
+
+
+        public void count()
+        {
+
+            intA++;
+        }
+
+
+        public static int getVal()
+        {
+
+            return intA;
+        }
+
+
+
+
+    }
+        
+
+    // Code for Inheritance 
+
+    class Shape
+    {
+
+        protected double  dblHeight;
+        protected double dblWidth;
+
+        public void setWidth(double w)
+        {
+
+            dblWidth = w;
+
+        }
+
+
+        public void SetHeight(double h)
+        {
+
+            dblHeight = h;
+
+        }
+
+      }
+
+
+    // Derivied CLass
+
+    class Rectangle : Shape
+    {
+
+        public double getArea()
+        {
+
+            return (dblWidth * dblHeight);
+
+        }
+
+        public void display()
+        {
+
+
+
+        }
+
+
+
+    }
+
+
+    // Base Class Initialization code 
+
+    class Square
+    {
+
+        protected double dblLength;
+        protected double dblWidth;
+
+
+        public Square(double l, double w)
+        {
+
+            dblLength = l;
+            dblWidth = w;
+
+
+        }
+
+
+
+        public double getArea()
+        {
+
+            return (dblLength * dblWidth);
+
+        }
+
+
+
+        public void display()
+        {
+
+            MessageBox.Show ("The Area is " + getArea ());
+
+
+        }
+    }
+
+    class BigSquare : Square
+    {
+
+        private double dblCost;
+
+        public BigSquare(double l, double w) 
+            : base(l, w)
+
+        {
+
+        }
+
+
+        public double getCost()
+        {
+
+            dblCost = getArea() * 10;
+            return dblCost;
+
+        }
+
+        public void display()
+        {
+
+            base.display ();
+            MessageBox.Show ("The Total Cost is " + getCost ().ToString ());
+            
+        }
+
+
+
+        }
+
+// Code for Multiple Intheritance 
+
+
+
+    class BaseClass
+    {
+
+        protected double dblLength;
+        protected double dblwidth;
+
+        public void setLength(double l)
+        {
+            dblLength = l;
+        }
+
+        public void setWidth(double w)
+        {
+
+            dblwidth = w;
+        }
+
+        
+    }
+
+    public interface InterTest
+    {
+
+      double  getVal ();
+
+    }
+
+
+    class DerivedTest : BaseClass, InterTest
+    {
+
+
+
+        public double getVal()
+        {
+
+            return (dblLength * dblwidth);
+        
+        }
+
+    }
+
+
+
+        
+}
+
+
